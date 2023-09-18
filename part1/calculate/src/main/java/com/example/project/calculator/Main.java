91f0a85526c4f9df4f950011a2af97dc2a7ecd07
jk <akdirdl@gmail.com>
2023-09-18 23:53:56 +0900

    feat: 서버 설정 및 http 프로토콜 출력

---
+++ part1/calculate/src/main/java/com/example/project/calculator/Main.java
@@ -0,0 +1,10 @@
+package com.example.project.calculator;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        new CustomWebApplicationServer(8080).start();
+
+    }
+}
