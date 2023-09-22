618a0c2b6dc8985c69c4ceba63d36bf4b5451942
jk <akdirdl@gmail.com>
2023-09-22 23:43:27 +0900

    HttpServlet

--- part1/servlet/src/main/java/com/example/project/CalculatorServlet.java
+++ part1/servlet/src/main/java/com/example/project/CalculatorServlet.java
@@ -5,26 +5,27 @@ import com.example.project.calculator.domain.PositiveNumber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet("/calculate")
-public class CalculatorServlet extends GenericServlet {
+public class CalculatorServlet extends HttpServlet {
     private static final Logger logger = LoggerFactory.getLogger(CalculatorServlet.class);
+
     @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         logger.info("service");
         int operand1 = Integer.parseInt(req.getParameter("operand1"));
         String operator = req.getParameter("operator");
         int operand2 = Integer.parseInt(req.getParameter("operand2"));
         int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
 
-        PrintWriter writer = res.getWriter();
+        PrintWriter writer = resp.getWriter();
         writer.println(result);
     }
 }
