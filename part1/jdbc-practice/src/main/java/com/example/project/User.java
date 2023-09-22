a721201d1da6e7b79bca299cc951aff09874ceae
jk <akdirdl@gmail.com>
2023-09-23 01:48:01 +0900

    jdbc 사용

---
+++ part1/jdbc-practice/src/main/java/com/example/project/User.java
@@ -0,0 +1,24 @@
+package com.example.project;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.Objects;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+public class User {
+    private final String userId;
+    private final String password;
+    private final String name;
+    private final String email;
+
+    public User(String userId, String password, String name, String email) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+}
